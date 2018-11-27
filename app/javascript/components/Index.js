ed8647f3fe3e6d9abe15a895edf4b88570664990
Saul Lopez <slopez@grupoabraxas.com>
2018-11-27 17:24:03 -0600

    Primer componente hecho

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -1,12 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
 class Index extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "si"
+    };
+    this.method = this.method.bind(this)
+  }
+  method() {
+    this.setState({name: "no"});
+  }
   render () {
     return (
-      <React.Fragment>
-        Junta: {this.props.junta}
-      </React.Fragment>
+    	<div>
+    	<h1>Estoy en Rails y uso React UuU</h1>
+        <button onClick={this.method}>accion</button>
+        <h2>{this.state.name}</h2>
       
+      </div>
+
     );
   }
 }
