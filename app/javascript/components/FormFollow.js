e5e223d0647d1d0e0acacdacf7e9811141b2232f
Saul Lopez <slopez@grupoabraxas.com>
2019-01-10 18:27:17 -0600

    problemas con el save de form

--- app/javascript/components/FormFollow.js
+++ app/javascript/components/FormFollow.js
@@ -5,24 +5,28 @@ class FormFollow extends React.Component {
         super(props);
         this.state = {
             body: this.props.follow_up.body,
-            date: this.props.follow_up.date						
+						date: this.props.follow_up.date,
+						meeting_id: this.props.follow_up.meeting_id					
 			};				
       this.methodB = this.methodB.bind(this)        			
-      this.methodD = this.methodD.bind(this)
+			this.methodD = this.methodD.bind(this)
+			this.methodM = this.methodM.bind(this)	
 	 }	
   methodB(event){
 		this.setState({body: event.target.value});
   } 
   methodD(event){
 		this.setState({date: event.target.value});
-  } 	 	
+	} 
+	methodM(event){
+		this.setState({meeting_id: event.target.value});
+	}	 	
 
     render(){
         return(				
 				<div>					          					
 					<input type="text" name="follow_up[body]" value={this.state.body} onChange={this.methodB}/>
-					<input type="date" name="follow_up[date]" value={this.state.date} onChange={this.methodD}/> 					
-          										
+					<input type="date" name="follow_up[date]" value={this.state.date} onChange={this.methodD}/> 										          										
         </div>        
         );
     }
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,8 +1,8 @@
 class Meeting < ApplicationRecord 
   belongs_to :user
-  has_many :follow_ups
+  has_many :follow_ups, dependent: :destroy 
 
-  accepts_nested_attributes_for :follow_ups 
+  accepts_nested_attributes_for :follow_ups   
   def to_s
     body
   end  
